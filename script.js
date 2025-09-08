fab7a4397cb637a4a04e84d227fa1d6627d75dc7
Austine <austineakhonya624@gmail.com>
2025-09-08 23:59:11 +0300

    js

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
-const nowVisible = ()=>{
-  const counters = document.querySelectorAll(".numes")
-
-counters.forEach(counter => {
-  let iterator = 0;
-  const clasAll = parseInt(counter.getAttribute("data-limit"));
-  
-  let increse = setInterval(()=>{
-    iterator++;
-    counter.textContent = iterator + "+";
-
-    if(iterator>=clasAll){
-        clearInterval(increse)
-    }
-
-  })
-});
-}
+const display = document.querySelector(".numes");
 
 
+let counter = 0;
+
+let interval = setInterval(()=>{
+  counter++;
+  display.textContent = counter;
+
+  if(counter>=250){
+    clearInterval(interval)
+  }
+}, 1)
