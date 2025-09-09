93b4d5396dbfc7172558ea38dd55c14f76049cb4
Austine <austineakhonya624@gmail.com>
2025-09-09 12:52:07 +0300

    10th and final Test

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ const totalDuration = 4000;
 const timeInterval = 10;
 const totalTime = totalDuration/timeInterval;
 
+
+
+
 nums.forEach(num=>{
   let counter = 0;
 
-  const attribute = parseInt(num.getAttribute("data-attribute"), 10);
+  const attribute = parseInt(num.getAttribute("data-attribute"));
 
   const interval = setInterval(()=>{
     counter++;
