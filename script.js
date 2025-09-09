0ded169e6c43e0c1c2b6b87ce55a116e86ed8cab
Austine <austineakhonya624@gmail.com>
2025-09-09 12:29:26 +0300

    &th success

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ const totalDuration = 4000;
 const timeInterval = 10;
 const totalTime = totalDuration/timeInterval;
 
-nums.forEach(num=>{
-  let counter = 0;
 
-  const attribute = parseInt(num.getAttribute("data-attribute"), 10);
-  
+nums.forEach(num=>{
+  let counter  = 0;
+
+  const attribute = parseInt(num.getAttribute("data-attribute"));
+
   const interval = setInterval(()=>{
     counter++;
     const progress = Math.min(Math.floor((attribute/totalTime)*counter), attribute);
 
-    num.textContent = `${progress}+`;
-  }, timeInterval)
-
-  if(counter>=totalTime){
-    clearInterval(interval)
-  }
+    num.textContent = `${progress}+`
+    if(counter>=totalTime){
+      clearInterval(interval)
+    }
+  })
 })
