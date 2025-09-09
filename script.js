93121e1e9ad36e40e59ff15c8a59e377bb0b46ff
Austine <austineakhonya624@gmail.com>
2025-09-09 03:26:44 +0300

    equal count

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const nums = document.querySelectorAll(".num-one");
 
 nums.forEach(num=>{
-  let counter = 0;
+let counter = 0;
 
   const limit = parseInt(num.getAttribute("data-attribute"));
 
+  const ttlDurarion = 2000;
+  const intervalTime = 10;
+  const totalSteps = ttlDurarion/intervalTime;
+
   let interval = setInterval(() => {
     counter++;
-    num.textContent = counter + "+";
+
+    const progress = Math.min(Math.floor((limit/totalSteps)*counter), limit);
+
+    num.textContent = progress + "+";
 
     if(counter>=limit){
       clearInterval(interval);
     }
-  }, 1);
+  }, intervalTime);
 })
